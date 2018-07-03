3ef8abff17fc20d76f4a56d3c7771a8005d21a4d
Jonathan <johnmorales990@gmail.com>
2018-07-03 18:00:26 -0300

    Adicionado CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,98 @@
 import React, { Component } from 'react';
-/*import logo from './logo.svg';*/
+import perfil from './img/01.jpg';
 import './css/App.scss';
-import 'font-awesome/css/font-awesome.min.css'; 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container-principal">
-        <div className="container-perfil">
 
-          <div className="container-perfil-left">
+	constructor(props) {
+		super(props);
+	}
 
-            <div className="perfil-card-background">
+	onClick = (index) => {
+		console.log(index)
+		console.log(`vc clicou no index: ${index}`);
+	}
 
-              <div className="perfil-card"></div>
+	render() {
+		return (
+			<div className="container-principal">
+				<div className="container-perfil">
 
-            </div>
+				<div className="container-perfil-left">
 
-          </div>
+					{/*
+					<ul>
+						{[1,2,3,4,5,6,7,8,9].map((item, index) => (
+							<li key={index} onClick={this.onClick.bind(this, index)}>{item}</li>
+						))}
+					</ul>
+					*/}
 
-          <div className="container-perfil-right">
+					<div className="perfil-card-background" onClick={this.onClick}>
+						<div className="perfil-image" style={{backgroundImage: `url(${perfil})`}} />
+					</div>
 
-            <button className="cria-taks-button"> <i className="fa fa-plus-circle button-icon"/> Adicionar Task </button>
+					<div className="container-perfil-status">
+						<span className="circle-status-perfil" />
+					</div>
 
-          </div>
+					<div className="container-perfil-dados">
 
-        </div>
-      </div>
-    );
-  }
+						<div className= "perfil-dados">
+							<p className= "perfil-nome">Lisa Helma Davoz</p>
+							<p className= "perfil-cargo">Senior Dev</p>
+						</div>
+
+						<div className= "perfil-dados-status-container">
+							<p className= "perfil-dados-titulo">Você possui:</p>
+
+							<div>
+								<p className= "perfil-dados-numero">02</p>
+								<p className= "perfil-dados-status">pendentes</p>
+							</div>
+
+							<div>
+								<p className= "perfil-dados-numero">01</p>
+								<p className= "perfil-dados-status">em produção</p>
+							</div> 
+
+							<div>
+								<p className= "perfil-dados-numero">01</p>
+								<p className= "perfil-dados-status">resolvidas</p>
+							</div> 
+
+						</div>
+
+					</div>
+
+				</div>
+
+				<div className="container-perfil-right">
+
+					<button className="cria-taks-button"> <span className="textButton">Adicionar Task</span> </button>
+
+				</div>
+
+				</div>
+
+				<div className="container-todo">
+					<span className="circle-pendentes"></span>
+					<p className = "titulo-todo"></p>
+				</div>
+
+				<div className="container-todo">
+					<span className="circle-emproducao"></span>
+					<p className = "titulo-todo"></p>
+				</div>
+
+				<div className="container-todo">
+					<span className="circle-resolvido"></span>
+					<p className = "titulo-todo"></p>
+				</div>
+
+			</div>
+			);
+	}
 }
 
 export default App;
