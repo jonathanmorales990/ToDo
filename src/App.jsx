8661b3d0c42793d65698155ad7b0ef8b79f220fd
jonathanmorales990 <you@example.com>
2018-07-04 00:16:22 -0300

    Ajustado

--- src/App.jsx
+++ src/App.jsx
@@ -16,79 +16,112 @@ class App extends Component {
 	render() {
 		return (
 			<div className="container-principal">
+
 				<div className="container-perfil">
 
-				<div className="container-perfil-left">
+  				<div className="container-perfil-left">
 
-					{/*
-					<ul>
-						{[1,2,3,4,5,6,7,8,9].map((item, index) => (
-							<li key={index} onClick={this.onClick.bind(this, index)}>{item}</li>
-						))}
-					</ul>
-					*/}
+  					{/*
+  					<ul>
+  						{[1,2,3,4,5,6,7,8,9].map((item, index) => (
+  							<li key={index} onClick={this.onClick.bind(this, index)}>{item}</li>
+  						))}
+  					</ul>
+  					*/}
 
-					<div className="perfil-card-background" onClick={this.onClick}>
-						<div className="perfil-image" style={{backgroundImage: `url(${perfil})`}} />
-					</div>
+  					<div className="perfil-card-background" onClick={this.onClick}>
+  						<div className="perfil-image" style={{backgroundImage: `url(${perfil})`}} />
+  					</div>
 
-					<div className="container-perfil-status">
-						<span className="circle-status-perfil" />
-					</div>
+  					<div className="container-perfil-status">
+  						<span className="circle-status-perfil" />
+  					</div>
 
-					<div className="container-perfil-dados">
+  					<div className="container-perfil-dados">
 
-						<div className= "perfil-dados">
-							<p className= "perfil-nome">Lisa Helma Davoz</p>
-							<p className= "perfil-cargo">Senior Dev</p>
-						</div>
+  						<div className= "perfil-dados">
+  							<p className= "perfil-nome">Lisa Helma Davoz</p>
+  							<p className= "perfil-cargo">Senior Dev</p>
+  						</div>
 
-						<div className= "perfil-dados-status-container">
-							<p className= "perfil-dados-titulo">Você possui:</p>
+  						<div className= "perfil-dados-status-container">
+  							<p className= "perfil-dados-titulo">Você possui:</p>
 
-							<div>
-								<p className= "perfil-dados-numero">02</p>
-								<p className= "perfil-dados-status">pendentes</p>
-							</div>
+  							<div>
+  								<p className= "perfil-dados-numero">02</p>
+  								<p className= "perfil-dados-status">pendentes</p>
+  							</div>
 
-							<div>
-								<p className= "perfil-dados-numero">01</p>
-								<p className= "perfil-dados-status">em produção</p>
-							</div> 
+  							<div>
+  								<p className= "perfil-dados-numero">01</p>
+  								<p className= "perfil-dados-status">em produção</p>
+  							</div> 
 
-							<div>
-								<p className= "perfil-dados-numero">01</p>
-								<p className= "perfil-dados-status">resolvidas</p>
-							</div> 
+  							<div>
+  								<p className= "perfil-dados-numero">01</p>
+  								<p className= "perfil-dados-status">resolvidas</p>
+  							</div> 
 
-						</div>
+  						</div>
 
-					</div>
+  					</div>
+
+  				</div>
+
+  				<div className="container-perfil-right">
+
+  					<button className="cria-taks-button"> <span className="textButton">Adicionar Task</span> </button>
+
+  				</div>
 
 				</div>
 
-				<div className="container-perfil-right">
+        <div className ="container-todo-principal">
+  				<div className="container-todo">
 
-					<button className="cria-taks-button"> <span className="textButton">Adicionar Task</span> </button>
+            <div>
+    					<span className="circle-pendentes"></span>
+    					<p className = "titulo-todo">Pendente</p>
+            </div>  
 
-				</div>
+  				</div>
 
-				</div>
+  				<div className="container-todo">
+            <div>
+    					<span className="circle-emproducao"></span>
+    					<p className = "titulo-todo">Em produção</p>
+            </div>
+            <div>    
+              <ul className="todo-lists-top-titulos">
+                <li className="todo-lists-top-empty"></li>
+                <li className="todo-lists-top-descricao">Descrição</li>
+                <li className="todo-lists-top-resposavel">Responsável</li>
+                <li className="todo-lists-top-status">Status</li>
 
-				<div className="container-todo">
-					<span className="circle-pendentes"></span>
-					<p className = "titulo-todo"></p>
-				</div>
+              </ul>
 
-				<div className="container-todo">
-					<span className="circle-emproducao"></span>
-					<p className = "titulo-todo"></p>
-				</div>
+            </div>
+            <div>
+              <ul className="todo-lists">
+                <li className="todo-lists-icon"><i className="icon-drag"></i></li>
+                <li className="todo-lists-descricao">Aerero Supofi Sfsafa</li>
+                <li className="todo-lists-responsavel"> 
+                  <span className="perfil-foto-mini"></span> 
+                  Lisa Helma Davoz
+                </li>
+                <li className="todo-lists-status"><span className="circle-emproducao-mini"></span>Em produção</li>
+                <li className="todo-lists-trash"><div><i className="icon-trash floatdir"/></div></li>
+              </ul>
+            </div>
+  				</div>
 
-				<div className="container-todo">
-					<span className="circle-resolvido"></span>
-					<p className = "titulo-todo"></p>
-				</div>
+  				<div className="container-todo">
+            <div>
+    					<span className="circle-resolvido"></span>
+    					<p className= "titulo-todo">Resolvido</p>
+            </div>
+  				</div>
+        </div>
 
 			</div>
 			);
